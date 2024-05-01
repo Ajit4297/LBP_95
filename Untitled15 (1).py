2ad53b5a277bade184528ee811a3ac3e5c49f565
Ajit4297 <168507427+Ajit4297@users.noreply.github.com>
2024-05-01 14:15:05 +0530

    Add files via upload

---
+++ Untitled15 (1).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+s = input().split() # ajit swain
+for  x in s:
+    if x !='':
+        print(x[0])
+  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
